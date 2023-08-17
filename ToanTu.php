025f9afa8710fdeaa50a1e01aa9b18b5b1d06359
LeVanTrung <142105875+trunglv2303@users.noreply.github.com>
2023-08-17 16:33:46 +0700

    Update ToanTu.php

--- ToanTu.php
+++ ToanTu.php
@@ -4,3 +4,57 @@ $a =1 ;
 $b =2;
 $const = $a +$b +10;
 
+/*Toán tử gán 
+- Toán tử =
+*/
+$age=40;
+const shop = 'http://shop.vn';
+echo $age;
+
+ /*Toán tử gán Toán tử .= */
+ $output='Học Lập Trình';
+ $output.='x2';
+ echo $output;
+ /*Toán tử lũy thừa ** */
+ echo '<br/>';
+ $a =2 ;// cơ số
+ $b =10;//số mũ
+ $result= $a**$b;
+ //=
+ echo $result;
+
+ /*Toán tử tanwng 1 đơn vị ++ */
+ $count=0;
+ $result =$count++;//$result =$count ; $count = $count +1;
+ $result =++$count;// $count = $count +1//$result =$count ;;
+
+
+
+ /*Toán tử giảm 1 đơn vị -- */
+ $count=2;
+//  $result =$count--;//$result =$count ; $count = $count -1;//=2
+ $result =++$count;// $count = $count -1//$result =$count ;;=1
+ echo $result;
+ echo '<br/>';
+
+  /*Toán tử so sánh */;
+  $a=10;
+  $b=20;
+  $check =$a>$b;
+  var_dump($check);
+  echo '<br/>';
+
+  /*Toán tử == */;
+  $a=10;
+  $b='10';
+  $check=$a==$b;
+  var_dump($check); //Chỉ so sánh giá trị
+
+  echo '<br/>';
+  /*Toán tử === */;
+  $a=10;
+  $b='10';
+  $check=$a==$b;
+  var_dump($check); //So sánh giá trị với kiểu dữ liệu
+
+
